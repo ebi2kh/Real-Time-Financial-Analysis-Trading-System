dcceb6d2c131fcc6330dcc37f1698056b2a3f8e5
ebi <eb.kh1372@gmail.com>
2024-01-31 00:39:09 +0330

    add more logic to Trading signal

--- Trading_Signal_Service/Trading_signal.js
+++ Trading_Signal_Service/Trading_signal.js
@@ -29,6 +29,71 @@
 
 // ------------------------------------------
 
+// const kafka = require("kafka-node");
+// const socketIO = require("socket.io");
+// const express = require("express");
+
+// const app = express();
+// const server = require("http").createServer(app);
+// const io = socketIO(server);
+
+// const Consumer = kafka.Consumer;
+// const client = new kafka.KafkaClient({ kafkaHost: "localhost:9092" });
+// const consumer = new Consumer(
+//   client,
+//   [{ topic: "Processed_data", partition: 0 }],
+//   { autoCommit: true }
+// );
+
+// consumer.on("message", function (message) {
+//   let data = JSON.parse(message.value);
+//   console.log("Received data: ", data);
+
+//   // Check if data_type field is not present
+//   if (!data.hasOwnProperty("data_type")) {
+//     let signal = calculateSignal(data);
+//     console.log("Signal: ", signal);
+//     io.sockets.emit("newdata", { data: signal });
+//   }
+// });
+
+// function calculateSignal(data) {
+//   let { moving_average, exponential_moving_average, rsi } = data;
+//   console.log(moving_average, exponential_moving_average, rsi);
+//   // Define your own conditions for buy, sell, and natural signals
+//   if (moving_average < exponential_moving_average || rsi < 30) {
+//     return "buy";
+//   } else if (moving_average > exponential_moving_average || rsi > 70) {
+//     return "sell";
+//   } else {
+//     return "natural";
+//   }
+// }
+
+// // consumer.on("message", function (message) {
+
+// //   console.log("Received data: " + message.value);
+// //   io.sockets.emit("newdata", { data: message.value });
+// // });
+
+// app.get("/events", function (req, res) {
+//   res.setHeader("Content-Type", "text/event-stream");
+//   res.setHeader("Cache-Control", "no-cache");
+//   res.setHeader("Connection", "keep-alive");
+//   res.flushHeaders();
+// });
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// server.listen(8000, function () {
+//   console.log("Server is running on port 8000");
+//   for (let i = 0; i < 10; i++) {
+//     io.sockets.emit("newdata", { data: "Hello World" });
+//   }
+// });
+// -------------------------------------------
+
 const kafka = require("kafka-node");
 const socketIO = require("socket.io");
 const express = require("express");
@@ -38,17 +103,53 @@ const server = require("http").createServer(app);
 const io = socketIO(server);
 
 const Consumer = kafka.Consumer;
+const Producer = kafka.Producer;
 const client = new kafka.KafkaClient({ kafkaHost: "localhost:9092" });
 const consumer = new Consumer(
   client,
   [{ topic: "Processed_data", partition: 0 }],
   { autoCommit: true }
 );
+const producer = new Producer(client);
 
 consumer.on("message", function (message) {
-  console.log("Received data: " + message.value);
-  io.sockets.emit("newdata", { data: message.value });
+  let data = JSON.parse(message.value);
+  console.log("Received data: ", data);
+
+  // Emit all data to the client
+  // io.sockets.emit("newdata", { data: data });
+
+  // Check if data_type field is not present
+  if (!data.hasOwnProperty("data_type")) {
+    let signal = calculateSignal(data);
+    console.log("Signal: ", signal);
+
+    // Add signal to data
+    data.signal = signal;
+
+    // Send data with signal to another Kafka topic
+    let payloads = [
+      { topic: "Signals", messages: JSON.stringify(data), partition: 0 },
+    ];
+    producer.send(payloads, function (err, data) {
+      console.log(data);
+    });
+  }
+  io.sockets.emit("newdata", { data: data });
 });
+
+function calculateSignal(data) {
+  let { moving_average, exponential_moving_average, rsi } = data;
+
+  // Define your own conditions for buy, sell, and natural signals
+  if (moving_average < exponential_moving_average && rsi < 30) {
+    return "buy";
+  } else if (moving_average > exponential_moving_average && rsi > 70) {
+    return "sell";
+  } else {
+    return "natural";
+  }
+}
 
 app.get("/events", function (req, res) {
   res.setHeader("Content-Type", "text/event-stream");
@@ -66,41 +167,3 @@ server.listen(8000, function () {
     io.sockets.emit("newdata", { data: "Hello World" });
   }
 });
-// -------------------------------------------
-
-// const kafka = require("kafka-node");
-// const express = require("express");
-
-// const app = express();
-
-// const Consumer = kafka.Consumer;
-// const client = new kafka.KafkaClient({ kafkaHost: "localhost:9092" });
-// const consumer = new Consumer(
-//   client,
-//   [{ topic: "Processed_data", partition: 0 }],
-//   { autoCommit: true }
-// );
-
-// app.get("/", function (req, res) {
-//   res.sendFile(__dirname + "/index.html");
-// });
-
-// app.get("/events", function (req, res) {
-//   res.setHeader("Content-Type", "text/event-stream");
-//   res.setHeader("Cache-Control", "no-cache");
-//   res.setHeader("Connection", "keep-alive");
-//   res.flushHeaders();
-
-//   consumer.on("message", function (message) {
-//     var data = message.value;
-//     console.log("Received data: " + data);
-//     res.write(`data: ${data}\n\n`);
-//   });
-// });
-
-// app.listen(8000, function () {
-//   console.log("Server is running on port 8000");
-//   for (let i = 0; i < 10; i++) {
-//     console.log("Hello World");
-//   }
-// });
